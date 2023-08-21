b5c714fcfa5e15fef5cb9f88594fd1d2defb4534
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-08-21 19:36:43 +0200

    added vievs and css

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.app")
+@section("title", "Home")
+
+@section("content")
+    <section>
+        <div>
+            <a href="{{route()}}">Register</a>
+        </div>
+        <div>
+            <a href="{{}}">Login</a>
+        </div>
+    </section>
+@endsection
